96a1db59a7add754d67cfa91b95517066e94048c
Zia <sajjalzia.inbox@gmail.com>
2024-06-05 01:20:52 +0500

    Polymorphism with Exercise Tracking (week 07)

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,138 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 3.0),
+            new Cycling("03 Nov 2022", 30, 18.0),
+            new Swimming("03 Nov 2022", 30, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
+
+abstract class Activity
+{
+    private string date;
+    private int minutes;
+
+    public Activity(string date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public string Date => date;
+    public int Minutes => minutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{Date} {this.GetType().Name} ({Minutes} min) - Distance: {GetDistance():F1} km, Speed: {GetSpeed():F1} kph, Pace: {GetPace():F2} min per km";
+    }
+}
+
+class Running : Activity
+{
+    private double distance; // distance in km
+
+    public Running(string date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{Date} Running ({Minutes} min) - Distance: {GetDistance():F1} km, Speed: {GetSpeed():F1} kph, Pace: {GetPace():F2} min per km";
+    }
+}
+
+class Cycling : Activity
+{
+    private double speed; // speed in kph
+
+    public Cycling(string date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (speed * Minutes) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{Date} Cycling ({Minutes} min) - Distance: {GetDistance():F1} km, Speed: {GetSpeed():F1} kph, Pace: {GetPace():F2} min per km";
+    }
+}
+
+class Swimming : Activity
+{
+    private int laps;
+    private const double LapDistanceKm = 50.0 / 1000.0; // distance of a lap in km
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * LapDistanceKm;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{Date} Swimming ({Minutes} min) - Distance: {GetDistance():F1} km, Speed: {GetSpeed():F1} kph, Pace: {GetPace():F2} min per km";
     }
 }
